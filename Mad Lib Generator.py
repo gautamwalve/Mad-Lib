2f429cb3dbad2dcceacf3096eeac73d212b2cafd
Aasura <gautamwalve@gmail.com>
2021-05-28 13:47:02 +0530

    ver 1.0

---
+++ Mad Lib Generator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h2>Mad-Lib Generator</h2>
+# <h6>Date: 26-May, 2021</h6>
+#     
+#  <h4>Story 01: A Day at the beach </h4>
+
+# In[3]:
+
+
+noun = input (" Name Someone: ")
+pronoun = input ("Describe them in one word!: ")
+plural_noun = input("What is their favorite food?: ")
+adjective = input ("one word for a cat in knight armor walking patrolling the block: ")
+verb = input("Life is....: ")
+
+print("\nA DAY AT THE BEACH. \nYesterday, i went to the beach. i had such a fun time!")
+print("When i arrived to the beach i couldnt help but notice the "+  adjective +" sand.")
+print("My Friend loved my " + adjective +" bathing suit i was wearing." )
+print("I looked at the ocean and couldnt believe my eyes! i saw " + plural_noun+" swimming around.")
+print("I wanted to go swimming with them but i was afraid they were going to " + verb + " me!")
+print("Instead I decided to build a " +noun+ " castle. I used a shovel, pail, and " +verb +" to help me build it.")
+print("The sun started to set and it was time to go home. I had the " + adjective +" time at the beach.")
+
+
+# In[ ]:
+
+
+
+
